b36da54a1b39d8752095959db96cd0cdccffe7a4
ozzly <gpg@ozzly.net>
2025-10-05 15:44:47 +0100

    format book info card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -5,17 +5,16 @@ function BookCard({ book:
  }) {
   return (
     <div className='book-card'>
-
-        
-        <h3>{title}</h3>
-        <p>{author_name}</p>
         <div className='book-card-image-container'>
-            <img src={cover_i ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg` : "/no-cover-image.png"} alt={title} />
-            <p className='book-card-overlay'>{edition_count} {edition_count>1 ? "editions" : " edition"}</p>
+            <img src={cover_i ? `https://covers.openlibrary.org/b/id/${cover_i}-L.jpg` : "/no-cover-image.png"} alt={title} />
+            <p onClick={() => console.log(edition_count)} className='book-card-overlay'>
+              {edition_count} {edition_count>1 ? "editions" : " edition"}
+            </p>
         </div>
-
-
-             
+        <h3 title={title} className='book-title'>{title}</h3>
+        <p title={author_name} className='book-author'>{author_name ? `By ${author_name}` : 'Unknown Author'}</p>
+        <p className='book-year'>{first_publish_year || 'N/A'}</p>
+        <button className='mark-as-read-button'>Mark as Read</button>
     </div>
   )
 }
