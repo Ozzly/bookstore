6d92d65295eb6d71784867fe23d14c60ef79fa30
ozzly <gpg@ozzly.net>
2025-10-07 17:52:43 +0100

    migrate search to tailwind

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Search({ searchTerm, setSearchTerm }) {
   return (
-    <div className='flex justify-center w-full'>
-    <div className='search-container'>
-      <div className='search-box'>
-        <input 
-            type='text'
-            placeholder='Search for your book'
+    <div className="flex justify-center w-full">
+      <div className="flex items-center border-3 border-ctp-surface0 rounded-xl focus-within:border-ctp-mauve transition-colors mb-3 mt-3">
+        <div className="p-2">
+          <input
+            type="text"
+            placeholder="Search for your book"
             value={searchTerm}
             onChange={(event) => setSearchTerm(event.target.value)}
-            className='search-input'
-        />
-        <div className='clear-button-container'>
-            <button onClick={() => setSearchTerm('')} className={searchTerm || "hidden"}>✕</button>
-
+            className="focus:outline-none w-md"
+          />
+          <button
+            onClick={() => setSearchTerm("")}
+            className={searchTerm || "hidden"}
+          >
+            ✕
+          </button>
+        </div>
+        <div className="flex items-center justify-center bg-ctp-surface0 h-full w-10 rounded-r-[9px] active:bg-ctp-surface1 transition-colors duration-600">
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+          </svg>
         </div>
       </div>
-        <div className='search-icon flex items-center justify-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search text-icon-neutral"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
-        </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
