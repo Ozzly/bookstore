b7264edf84cd3049863d4698da9c216681ba188e
ozzly <gpg@ozzly.net>
2025-10-07 12:14:04 +0100

    improve loading screen, move app.jsx over to tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import Search from './components/Search'
-import BookList from './components/BookList';
+import React, { useEffect, useState } from "react";
+import Search from "./components/Search";
+import BookList from "./components/BookList";
 import { useDebounce } from "use-debounce";
-
+import { BarLoader } from "react-spinners";
 
 const API_BASE_URL = "https://openlibrary.org";
 const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json'
-  }
-}
+    accept: "application/json",
+  },
+};
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm] = useDebounce(searchTerm, 1000);
   const [readBooks, setReadBooks] = useState(() => {
-    const stored = localStorage.getItem('readBooks');
+    const stored = localStorage.getItem("readBooks");
     return stored ? JSON.parse(stored) : [];
   });
   // const [readBooksDetails, setReadBooksDetails] = useState([]);
 
   useEffect(() => {
-    debouncedSearchTerm !== '' && fetchBooksQuery(debouncedSearchTerm);
-  }, [debouncedSearchTerm])
+    debouncedSearchTerm !== "" && fetchBooksQuery(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   useEffect(() => {
-    localStorage.setItem('readBooks', JSON.stringify(readBooks));
+    localStorage.setItem("readBooks", JSON.stringify(readBooks));
   }, [readBooks]);
 
   // useEffect(() => {
@@ -48,93 +48,103 @@ const App = () => {
   //   console.log(details);
   // }
 
-  const fetchBooksQuery = async (query = '') => {
+  const fetchBooksQuery = async (query = "") => {
     setIsLoading(true);
-    setErrorMessage('');
+    setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/search.json?title=${encodeURIComponent(query)}`;
+      const endpoint = `${API_BASE_URL}/search.json?title=${encodeURIComponent(
+        query
+      )}`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
-        throw new Error('Failed to fetch books');
+        throw new Error("Failed to fetch books");
       }
 
       const data = await response.json();
       console.log(data);
 
-      if (data.Response === 'False') {
+      if (data.Response === "False") {
         setErrorMessage(data.Error || "Failed to fetch movies");
         setBooks([]);
         return;
       }
 
-      setBooks(data.docs || [])
-
+      setBooks(data.docs || []);
     } catch (error) {
       console.log(`Error fetching book: ${error}`);
-      setErrorMessage('Error fetching books.');
+      setErrorMessage("Error fetching books.");
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   const toggleReadStatus = (book) => {
     setReadBooks((prevReadBooks) => {
-      const exists = prevReadBooks.some(b => b.key === book.key);
+      const exists = prevReadBooks.some((b) => b.key === book.key);
 
       if (exists) {
         return prevReadBooks.filter((b) => b.key !== book.key);
       } else {
-        return [...prevReadBooks, {
-          key: book.key, 
-          title: book.title, 
-          author_name: book.author_name, 
-          cover_i: book.cover_i,
-          edition_count: book.edition_count,
-          first_publish_year: book.first_publish_year,
-        }];
+        return [
+          ...prevReadBooks,
+          {
+            key: book.key,
+            title: book.title,
+            author_name: book.author_name,
+            cover_i: book.cover_i,
+            edition_count: book.edition_count,
+            first_publish_year: book.first_publish_year,
+          },
+        ];
       }
     });
-  }
-
-  
+  };
 
   return (
-    <div className='app-container'>
-    <main>
-      <div className='app-header'>
+    <div className="flex items-center flex-col text-ctp-text">
+      <div className="w-full border-b-2 border-ctp-overlay0 sticky top-0 bg-ctp-base z-10 mb-3">
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-
       </div>
-
-      {/* <div className='books-summary-section'>
-        <h2>Read Books</h2>
-        <div>
-          <BookList books={readBooks} readBooks={readBooks} toggleReadStatus={toggleReadStatus} />
-        </div>
-      </div> */}
-      
-      <div>
-        <h2>All Books</h2>
-        {debouncedSearchTerm == '' ? (
+      <main className="w-full max-w-6xl">
+        {debouncedSearchTerm == "" ? (
           <>
-            <p>Showing Read Books</p>
-            <BookList books={readBooks} readBooks={readBooks} toggleReadStatus={toggleReadStatus} />
-
+            <h2 className="text-xl font-bold text-center mb-2">
+              Finished Books
+            </h2>
+            <BookList
+              books={readBooks}
+              readBooks={readBooks}
+              toggleReadStatus={toggleReadStatus}
+            />
           </>
-          
         ) : isLoading ? (
-          <p>Loading...</p>
+          <>
+            <h2 className="text-xl font-bold text-center mb-6">
+              Fetching Results for "{debouncedSearchTerm}"
+            </h2>
+            <div className="justify-center flex">
+              <BarLoader loading="true" color="#cba6f7" width="50%" />
+            </div>
+          </>
         ) : errorMessage ? (
-          <p className='text-red-500'>{errorMessage}</p>
+          <p className="text-ctp-red">{errorMessage}</p>
         ) : (
-          <BookList books={books} readBooks={readBooks} toggleReadStatus={toggleReadStatus} />
-          
-        )}
-      </div>
-    </main>
-    </div>
-  )
-}
+          <>
+            <h2 className="text-xl font-bold text-center mb-2">
+              Search Results for "{debouncedSearchTerm}"
+            </h2>
 
-export default App
+            <BookList
+              books={books}
+              readBooks={readBooks}
+              toggleReadStatus={toggleReadStatus}
+            />
+          </>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -16,7 +16,7 @@ function Search({ searchTerm, setSearchTerm }) {
             <button onClick={() => setSearchTerm('')} className={searchTerm || "hidden"}>✕</button>
 
         </div>
-        </div>
+      </div>
         <div className='search-icon flex items-center justify-center'>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search text-icon-neutral"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
         </div>
