5e35c18e8499e1ba06fc85065c32c503b2b109ee
ozzly <gpg@ozzly.net>
2025-10-11 21:24:24 +0100

    clean home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,9 @@
 import React, { useEffect, useState } from "react";
-import Search from "./Search.js";
 import BookSearchList from "./BookSearchList.js";
 import { BarLoader } from "react-spinners";
-import type { Book } from "../types.js";
 import { useSearchStore } from "../store.js";
 
-const API_BASE_URL = "https://openlibrary.org";
-const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-  },
-};
-
 const App = () => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [books, setBooks] = useState([]);
-  // const [debouncedSearchTerm] = useDebounce(searchTerm, 1000);
   const [readBooks, setReadBooks] = useState(() => {
     const stored = localStorage.getItem("readBooks");
     return stored ? JSON.parse(stored) : [];
@@ -26,10 +13,6 @@ const App = () => {
     return stored ? JSON.parse(stored) : [];
   });
 
-  // useEffect(() => {
-  //   debouncedSearchTerm !== "" && fetchBooksQuery(debouncedSearchTerm);
-  // }, [debouncedSearchTerm]);
-
   useEffect(() => {
     localStorage.setItem("readBooks", JSON.stringify(readBooks));
   }, [readBooks]);
@@ -37,66 +20,6 @@ const App = () => {
   useEffect(() => {
     localStorage.setItem("planToReadBooks", JSON.stringify(planToReadBooks));
   }, [planToReadBooks]);
-
-  // useEffect(() => {
-  //   readBooks.length === 0 ? setReadBooksDetails([]) : fetchWorksByKeys(readBooks);
-  // }, [readBooks, debouncedSearchTerm]);
-
-  // const fetchWorksByKeys = async (keys) => {
-  //   const details = await Promise.all(
-  //     keys.map (async (key) => {
-  //       const response = await fetch(`${API_BASE_URL}${key}.json`, API_OPTIONS);
-  //       if (!response.ok) return null;
-  //       return await response.json();
-  //     })
-  //   );
-  //   setReadBooksDetails(details);
-  //   console.log(details);
-  // }
-
-  const toggleReadStatus = (book: Book) => {
-    setReadBooks((prevReadBooks: [Book]) => {
-      const exists = prevReadBooks.some((b) => b.key === book.key);
-
-      if (exists) {
-        return prevReadBooks.filter((b) => b.key !== book.key);
-      } else {
-        return [
-          ...prevReadBooks,
-          {
-            key: book.key,
-            title: book.title,
-            author_name: book.author_name,
-            cover_i: book.cover_i,
-            edition_count: book.edition_count,
-            first_publish_year: book.first_publish_year,
-          },
-        ];
-      }
-    });
-  };
-
-  const togglePlanToReadStatus = (book: Book) => {
-    setPlanToReadBooks((prevPlanToReadBooks: [Book]) => {
-      const exists = prevPlanToReadBooks.some((b) => b.key === book.key);
-
-      if (exists) {
-        return prevPlanToReadBooks.filter((b) => b.key !== book.key);
-      } else {
-        return [
-          ...prevPlanToReadBooks,
-          {
-            key: book.key,
-            title: book.title,
-            author_name: book.author_name,
-            cover_i: book.cover_i,
-            edition_count: book.edition_count,
-            first_publish_year: book.first_publish_year,
-          },
-        ];
-      }
-    });
-  };
 
   const booksFromStore = useSearchStore((state) => state.books);
   const searchTerm = useSearchStore((state) => state.searchTerm);
@@ -115,8 +38,6 @@ const App = () => {
               <BarLoader loading={true} color="#cba6f7" width="50%" />
             </div>
           </>
-        ) : errorMessage ? (
-          <p className="text-ctp-red">{errorMessage}</p>
         ) : (
           <>
             <h2 className="text-xl font-bold text-center mb-2">
