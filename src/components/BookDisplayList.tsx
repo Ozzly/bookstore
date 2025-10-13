5917f77ea964013c45615ac5e090b6e559fcea5b
ozzly <gpg@ozzly.net>
2025-10-13 12:51:52 +0100

    update responsive width for book summary page

--- src/components/BookDisplayList.tsx
+++ src/components/BookDisplayList.tsx
@@ -7,31 +7,33 @@ function BookDisplayList() {
   const plannedBooks = useLibraryStore((state) => state.planToReadBooks);
 
   return (
-    <div className="w-full flex flex-col px-16 gap-4">
-      <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col">
-        <div className="bold text-xl text-ctp-text">Completed Books</div>
-        <div className="flex gap-4">
-          {completedBooks.map((book) => (
-            <BookCard
-              key={book.key}
-              book={book}
-              isMarkedRead={true}
-              isPlannedToRead={false}
-            />
-          ))}
+    <div className="flex justify-center">
+      <div className="w-full max-w-6xl flex flex-col px-16 gap-4">
+        <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col w-full">
+          <div className="bold text-xl text-ctp-text">Completed Books</div>
+          <div className="flex overflow-y-scroll gap-4 w-full py-2">
+            {completedBooks.map((book) => (
+              <BookCard
+                key={book.key}
+                book={book}
+                isMarkedRead={true}
+                isPlannedToRead={false}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col">
-        <div className="bold text-xl text-ctp-text">Planned to Read</div>
-        <div className="flex gap-4">
-          {plannedBooks.map((book) => (
-            <BookCard
-              key={book.key}
-              book={book}
-              isMarkedRead={false}
-              isPlannedToRead={true}
-            />
-          ))}
+        <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col w-full">
+          <div className="bold text-xl text-ctp-text">Planned to Read</div>
+          <div className="flex overflow-y-scroll gap-4 w-full py-2">
+            {plannedBooks.map((book) => (
+              <BookCard
+                key={book.key}
+                book={book}
+                isMarkedRead={false}
+                isPlannedToRead={true}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
