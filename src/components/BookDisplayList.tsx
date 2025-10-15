a6f4a3370dde2225cf3d6b5bc942b06cf226e83b
ozzly <gpg@ozzly.net>
2025-10-15 09:15:04 +0100

    make BookDisplayList into a proper component rather than acting as a page

--- src/components/BookDisplayList.tsx
+++ src/components/BookDisplayList.tsx
@@ -1,36 +1,33 @@
 import React from "react";
 import BookCard from "./BookCard.js";
 import { useBookStore } from "../stores/bookStore.js";
+import type { Book } from "../types.js";
 
-function BookDisplayList() {
-  const completedBooks = useBookStore((state) => state.completedBooks);
-  const plannedBooks = useBookStore((state) => state.planToReadBooks);
+interface BookDisplayListProps {
+  books: Book[];
+  title: string;
+  isMarkedRead: boolean; // Should add an array of groups/categories this book belong to later, letting this be removed
+  isPlannedToRead: boolean;
+}
 
+function BookDisplayList({
+  books,
+  title,
+  isMarkedRead,
+  isPlannedToRead,
+}: BookDisplayListProps) {
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-6xl flex flex-col px-16 gap-4">
         <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col w-full">
-          <div className="bold text-xl text-ctp-text">Completed Books</div>
+          <div className="bold text-xl text-ctp-text">{title}</div>
           <div className="flex overflow-y-scroll gap-4 w-full py-2">
-            {completedBooks.map((book) => (
+            {books.map((book) => (
               <BookCard
                 key={book.key}
                 book={book}
-                isMarkedRead={true}
-                isPlannedToRead={false}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="border-2 border-ctp-surface1 rounded-lg p-2 items-center flex flex-col w-full">
-          <div className="bold text-xl text-ctp-text">Planned to Read</div>
-          <div className="flex overflow-y-scroll gap-4 w-full py-2">
-            {plannedBooks.map((book) => (
-              <BookCard
-                key={book.key}
-                book={book}
-                isMarkedRead={false}
-                isPlannedToRead={true}
+                isMarkedRead={isMarkedRead}
+                isPlannedToRead={isPlannedToRead}
               />
             ))}
           </div>
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import BookDisplayList from "../components/BookDisplayList.js";
+import { useBookStore } from "../stores/bookStore.js";
 
 function Books() {
-  return <BookDisplayList />;
+  const completedBooks = useBookStore((state) => state.completedBooks);
+  const plannedBooks = useBookStore((state) => state.planToReadBooks);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <BookDisplayList
+        books={completedBooks}
+        title="Completed Books"
+        isMarkedRead={true}
+        isPlannedToRead={false}
+      />
+      <BookDisplayList
+        books={plannedBooks}
+        title="Planned to Read"
+        isMarkedRead={false}
+        isPlannedToRead={true}
+      />
+    </div>
+  );
 }
 
 export default Books;
