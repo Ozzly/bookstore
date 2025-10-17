5865e11d780e92d982ff23c034ff500add9954cd
ozzly <gpg@ozzly.net>
2025-10-17 22:35:02 +0100

    switch anime cards to horizontal

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -8,16 +8,16 @@ interface AnimeCardProps {
 function AnimeCard({ anime }: AnimeCardProps) {
   const { mal_id, title, score, cover_image, episodes, year } = anime;
   return (
-    <div className="w-[400px] h-[220px] p-4 bg-ctp-surface0 rounded-lg border-ctp-surface1 hover:scale-102 hover:shadow-xl transition shadow-lg">
-      <div className="flex h-full">
-        <div className="w-1/3 h-full flex">
+    <div className="size-fit p-4 bg-ctp-surface0 rounded-lg border-ctp-surface1 hover:scale-102 hover:shadow-xl transition shadow-lg">
+      <div className="flex h-full gap-4 text-ctp-text">
+        <div className="w-[180px] h-[256px]">
           <img
             src={cover_image}
             alt={title}
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="text-ctp-text">
+        <div className="w-[200px]">
           <h2 className="font-bold">{title}</h2>
         </div>
       </div>
