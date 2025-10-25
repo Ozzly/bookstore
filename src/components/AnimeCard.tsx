14dcd391fcaba673a08bf37ffe0f076063fd82ee
ozzly <gpg@ozzly.net>
2025-10-25 12:45:33 +0100

    move mark button logic to AnimeCard

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -1,5 +1,12 @@
 import type { ReactNode } from "react";
 import type { Anime } from "../types.js";
+import { DropdownMenu } from "radix-ui";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  PlusCircledIcon,
+} from "@radix-ui/react-icons";
+import { useAnimeStore } from "../stores/animeStore.js";
 
 interface AnimeCardProps {
   anime: Anime;
@@ -17,6 +24,9 @@ function AnimeCard({ anime, children }: AnimeCardProps) {
     release_season,
     studio,
   } = anime;
+
+  const addWatchedAnime = useAnimeStore((state) => state.addWatchedAnime);
+
   return (
     <div className="size-fit p-4 bg-ctp-surface0 rounded-lg border-ctp-surface1 hover:scale-102 hover:shadow-xl transition shadow-lg">
       <div className="flex h-full gap-4 text-ctp-text">
@@ -39,7 +49,58 @@ function AnimeCard({ anime, children }: AnimeCardProps) {
           <p className="text-center">
             {episodes || "N/A"} {episodes > 1 ? "episodes" : "episode"}
           </p>
-          <div className="absolute bottom-0">{children}</div>
+          <div className="absolute bottom-0">
+            <div className="">
+              <button
+                className="border-3 border-ctp-mauve rounded-l-lg p-1 border-r-2"
+                onClick={() => addWatchedAnime(anime)}
+              >
+                Mark as Watched
+              </button>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <button className="text-ctp-mauve border-3 border-l-2 border-ctp-mauve rounded-r-lg p-1 px-2 font-bold">
+                    +
+                  </button>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Portal>
+                  <DropdownMenu.Content
+                    className="bg-ctp-base p-2 rounded-lg shadow-lg border-ctp-surface1 border-3 text-ctp-text"
+                    side="top"
+                    align="end"
+                  >
+                    <DropdownMenu.Label />
+                    <DropdownMenu.Item />
+
+                    <DropdownMenu.RadioGroup>
+                      <DropdownMenu.RadioItem
+                        value="watched"
+                        className="hover:bg-ctp-surface1 p-1 rounded-md"
+                      >
+                        Mark as Watched
+                        <DropdownMenu.ItemIndicator />
+                      </DropdownMenu.RadioItem>
+                      <DropdownMenu.RadioItem
+                        value="watching"
+                        className="hover:bg-ctp-surface2 p-1 rounded-md"
+                      >
+                        Mark as Watching
+                        <DropdownMenu.ItemIndicator />
+                      </DropdownMenu.RadioItem>
+                      <DropdownMenu.RadioItem
+                        value="planToWatch"
+                        className="hover:bg-ctp-surface2 p-1 rounded-md"
+                      >
+                        Plan to Watch
+                        <DropdownMenu.ItemIndicator />
+                      </DropdownMenu.RadioItem>
+                    </DropdownMenu.RadioGroup>
+                  </DropdownMenu.Content>
+                </DropdownMenu.Portal>
+              </DropdownMenu.Root>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,72 +1,14 @@
 import React from "react";
 import { useAnimeStore } from "../stores/animeStore.js";
 import AnimeCard from "../components/AnimeCard.js";
-import { DropdownMenu } from "radix-ui";
-import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  PlusCircledIcon,
-} from "@radix-ui/react-icons";
 
 function Anime() {
   const animeSearchResults = useAnimeStore((state) => state.animeResults);
-  const addWatchedAnime = useAnimeStore((state) => state.addWatchedAnime);
   return (
     <div className="flex justify-center">
       <div className="flex flex-wrap justify-center gap-3 w-[1600px]">
         {animeSearchResults.map((anime) => (
-          <AnimeCard key={anime.mal_id} anime={anime}>
-            <div className="">
-              <button
-                className="border-3 border-ctp-mauve rounded-l-lg p-1 border-r-2"
-                onClick={() => addWatchedAnime(anime)}
-              >
-                Mark as Watched
-              </button>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <button className="text-ctp-mauve border-3 border-l-2 border-ctp-mauve rounded-r-lg p-1 px-2 font-bold">
-                    +
-                  </button>
-                </DropdownMenu.Trigger>
-
-                <DropdownMenu.Portal>
-                  <DropdownMenu.Content
-                    className="bg-ctp-base p-2 rounded-lg shadow-lg border-ctp-surface1 border-3 text-ctp-text"
-                    side="top"
-                    align="end"
-                  >
-                    <DropdownMenu.Label />
-                    <DropdownMenu.Item />
-
-                    <DropdownMenu.RadioGroup>
-                      <DropdownMenu.RadioItem
-                        value="watched"
-                        className="hover:bg-ctp-surface1 p-1 rounded-md"
-                      >
-                        Mark as Watched
-                        <DropdownMenu.ItemIndicator />
-                      </DropdownMenu.RadioItem>
-                      <DropdownMenu.RadioItem
-                        value="watching"
-                        className="hover:bg-ctp-surface2 p-1 rounded-md"
-                      >
-                        Mark as Watching
-                        <DropdownMenu.ItemIndicator />
-                      </DropdownMenu.RadioItem>
-                      <DropdownMenu.RadioItem
-                        value="planToWatch"
-                        className="hover:bg-ctp-surface2 p-1 rounded-md"
-                      >
-                        Plan to Watch
-                        <DropdownMenu.ItemIndicator />
-                      </DropdownMenu.RadioItem>
-                    </DropdownMenu.RadioGroup>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Portal>
-              </DropdownMenu.Root>
-            </div>
-          </AnimeCard>
+          <AnimeCard key={anime.mal_id} anime={anime} />
         ))}
       </div>
     </div>
