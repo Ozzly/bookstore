8f3eb50e6881200c13e27c7f2e3911a492d229fb
ozzly <gpg@ozzly.net>
2025-10-27 18:54:49 +0000

    move navigation to sidebar, create component for nav item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,47 +16,6 @@ function Header() {
 
   return (
     <div className="w-full border-b-1 border-ctp-surface0 sticky top-0 bg-ctp-base z-10 text-ctp-text flex justify-center">
-      <div className="grid grid-cols-6 items-center text-center divide-x-1 divide divide-ctp-surface1 px-4">
-        <NavLink to="/">
-          {({ isActive }) => (
-            <div
-              className={`py-2 hover:bg-ctp-surface0 ${
-                isActive ? "font-bold text-ctp-mauve" : ""
-              } `}
-            >
-              Search
-            </div>
-          )}
-        </NavLink>
-        <NavLink to="/books" onClick={() => setSearchCategory("books")}>
-          {({ isActive }) => (
-            <div
-              className={`py-2 hover:bg-ctp-surface0 ${
-                isActive ? "font-bold text-ctp-mauve" : ""
-              } `}
-            >
-              Books
-            </div>
-          )}
-        </NavLink>
-        <NavLink to="/anime" onClick={() => setSearchCategory("anime")}>
-          {({ isActive }) => (
-            <div
-              className={`py-2 hover:bg-ctp-surface0 ${
-                isActive ? "font-bold text-ctp-mauve" : ""
-              } `}
-            >
-              Anime
-            </div>
-          )}
-        </NavLink>
-        <div className="py-2 hover:bg-ctp-surface0 line-through">Manga</div>
-        <div className="py-2 hover:bg-ctp-surface0 line-through">Movies</div>
-        <div className="pl-2 py-2 hover:bg-ctp-surface0 line-through">
-          TV Shows
-        </div>
-      </div>
-
       <Search />
 
       <Select.Root
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { NavLink } from "react-router";
+import { useSearchStore } from "../stores/searchUIStore.js";
+
+type Page = "books" | "anime" | "manga" | "movies" | "shows";
 
 function Sidebar() {
+  const setSearchCategory = useSearchStore((state) => state.setSearchCategory);
+
   return (
     <div className="w-64 flex-shrink-0 h-full border-r-1 border-ctp-surface0">
-      Sidebar
+      <div className="flex flex-col m-3 text-ctp-text">
+        <SidebarPageItem page="books" />
+        <SidebarPageItem page="anime" />
+        <div className="py-2 hover:bg-ctp-surface0 line-through">Manga</div>
+        <div className="py-2 hover:bg-ctp-surface0 line-through">Movies</div>
+        <div className="pl-2 py-2 hover:bg-ctp-surface0 line-through">
+          TV Shows
+        </div>
+      </div>
     </div>
+  );
+}
+
+interface SidebarPageItemProps {
+  page: Page;
+}
+
+function SidebarPageItem({ page }: SidebarPageItemProps) {
+  const setSearchCategory = useSearchStore((state) => state.setSearchCategory);
+  return (
+    <NavLink to={`/${page}`} onClick={() => setSearchCategory(page)}>
+      {({ isActive }) => (
+        <div
+          className={`p-1 hover:bg-ctp-surface0 w-full rounded-lg ${
+            isActive && "font-bold bg-ctp-surface1"
+          } `}
+        >
+          {page}
+        </div>
+      )}
+    </NavLink>
   );
 }
 
