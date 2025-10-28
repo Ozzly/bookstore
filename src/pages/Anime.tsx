3a4b7b4649660e4cb6eadf0107b5ae4405a2829e
ozzly <gpg@ozzly.net>
2025-10-28 22:33:32 +0000

    display watched anime when there is no search term

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useAnimeStore } from "../stores/animeStore.js";
 import AnimeCard from "../components/AnimeCard.js";
+import { useSearchStore } from "../stores/searchUIStore.js";
 
 function Anime() {
   const animeSearchResults = useAnimeStore((state) => state.animeResults);
+  const searchTerm = useSearchStore((state) => state.searchTerm);
+  const watchedAnime = useAnimeStore((state) => state.animeWatched);
+
   return (
     <div className="flex justify-center mt-3">
       <div className="flex flex-wrap justify-center gap-3">
-        {animeSearchResults.map((anime) => (
-          <AnimeCard key={anime.mal_id} anime={anime} />
-        ))}
+        {searchTerm === ""
+          ? watchedAnime.map((anime) => (
+              <AnimeCard key={anime.mal_id} anime={anime} />
+            ))
+          : animeSearchResults.map((anime) => (
+              <AnimeCard key={anime.mal_id} anime={anime} />
+            ))}
       </div>
     </div>
   );
