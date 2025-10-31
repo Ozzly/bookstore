2e23270bf0b1fe949f79f23ed1cce2ef8001eab9
ozzly <gpg@ozzly.net>
2025-10-31 12:21:55 +0000

    add filter by category buttons to Anime home page

--- src/pages/Anime.tsx
+++ src/pages/Anime.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAnimeStore } from "../stores/animeStore.js";
 import AnimeCard from "../components/AnimeCard.js";
 import { useSearchStore } from "../stores/searchUIStore.js";
 import { BarLoader } from "react-spinners";
+import { ToggleGroup } from "radix-ui";
+import {
+  TextAlignLeftIcon,
+  TextAlignCenterIcon,
+  TextAlignRightIcon,
+} from "@radix-ui/react-icons";
+
+function ToggleGroupItem({ value }: { value: string }) {
+  return (
+    <ToggleGroup.Item
+      value={value}
+      aria-label={`Select ${value} Anime`}
+      className="text-ctp-mauve border-3 border-ctp-mauve rounded-lg px-4 py-1  data-[state=on]:bg-ctp-mauve data-[state=on]:text-ctp-base"
+    >
+      {value}
+    </ToggleGroup.Item>
+  );
+}
 
 function Anime() {
   const animeSearchResults = useAnimeStore((state) => state.animeResults);
@@ -12,16 +30,56 @@ function Anime() {
   const plannedAnime = useAnimeStore((state) => state.animePlanned);
   const isLoading = useSearchStore((state) => state.isLoading);
 
-  const allAnime = [...watchedAnime, ...watchingAnime, ...plannedAnime];
-  allAnime.sort((a, b) => a.title.localeCompare(b.title));
+  const [animeSummary, setAnimeSummary] = React.useState([
+    ...watchedAnime,
+    ...watchingAnime,
+    ...plannedAnime,
+  ]);
+
+  const [filterStatus, setFilterStatus] = React.useState("all");
+  useEffect(() => {
+    switch (filterStatus) {
+      case "All":
+        setAnimeSummary([...watchedAnime, ...watchingAnime, ...plannedAnime]);
+        break;
+      case "Watched":
+        setAnimeSummary([...watchedAnime]);
+        break;
+      case "Watching":
+        setAnimeSummary([...watchingAnime]);
+        break;
+      case "Planned":
+        setAnimeSummary([...plannedAnime]);
+        break;
+    }
+  }, [filterStatus]);
 
   return (
     <div className="flex justify-center">
       {searchTerm === "" ? (
-        <div className="flex flex-wrap justify-center gap-3">
-          {allAnime.map((anime) => (
-            <AnimeCard key={anime.mal_id} anime={anime} />
-          ))}
+        <div>
+          <div>
+            <ToggleGroup.Root
+              type="single"
+              defaultValue="all"
+              value={filterStatus}
+              onValueChange={(value) => setFilterStatus(value)}
+              className="flex gap-2"
+            >
+              <ToggleGroupItem value="All" />
+              <ToggleGroupItem value="Watched" />
+              <ToggleGroupItem value="Watching" />
+              <ToggleGroupItem value="Planned" />
+            </ToggleGroup.Root>
+          </div>
+          <div className="text-ctp-text text-center font-bold text-xl mb-4">
+            Your Anime Collection
+          </div>
+          <div className="flex flex-wrap justify-center gap-3">
+            {animeSummary.map((anime) => (
+              <AnimeCard key={anime.mal_id} anime={anime} />
+            ))}
+          </div>
         </div>
       ) : isLoading ? (
         <div>
