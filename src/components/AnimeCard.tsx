8cefa19cd00f1ca290514b2a04a8b4d79745c1b8
ozzly <gpg@ozzly.net>
2025-11-06 09:36:03 +0000

    use MediaCard for AnimeCard

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -3,6 +3,7 @@ import { useAnimeStore } from "../stores/animeStore.js";
 import { FaStar } from "react-icons/fa";
 import StatusButton from "./StatusButton.js";
 import NumberInputWithButtons from "./NumberInputWithButtons.js";
+import MediaCard from "./MediaCard.js";
 interface AnimeCardProps {
   item: Anime;
 }
@@ -52,90 +53,79 @@ function AnimeCard({ item }: AnimeCardProps) {
   }
 
   return (
-    <div className="size-fit p-4 bg-ctp-surface0 rounded-lg border-ctp-surface1 hover:scale-102 hover:shadow-xl transition shadow-lg group/card">
-      <div className="flex h-full gap-4 text-ctp-text">
-        <div className="w-[180px] h-[256px]">
-          <img
-            src={cover_image}
-            alt={title}
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <div className="w-[190px] relative">
-          <h2 className="font-bold line-clamp-2 leading-tight" title={title}>
-            {title}
-          </h2>
-          <p className="text-sm mb-1">{studio}</p>
-          <p>
-            {release_season} {year}
-          </p>
-          <div
-            className={
-              score >= 8
-                ? "text-ctp-green"
-                : score >= 6
-                ? "text-ctp-yellow"
-                : score >= 5
-                ? "text-ctp-maroon"
-                : score === null
-                ? "text-ctp-subtext0"
-                : "text-ctp-red"
-            }
-          >
-            <div className="flex items-center gap-1">
-              {score || "N/A"} <FaStar />
-            </div>
-          </div>
-          <p>
-            {episodes || "N/A"} {videoType === "TV" ? "Episodes" : videoType}
-          </p>
-
-          <div className="h-12 overflow-hidden mt-1">
-            <div className="flex flex-wrap gap-1">
-              {themes?.map((theme: string) => (
-                <div
-                  key={theme}
-                  className="text-xs bg-ctp-surface2 rounded-xl w-fit px-2 py-0.5"
-                >
-                  {theme}
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <div className="absolute bottom-0 w-full">
-            {currentStatus === "completed" ? (
-              <div className="text-ctp-subtext0 text-center">
-                Finished {dateAdded}
-              </div>
-            ) : (
-              currentStatus === "progress" && (
-                <div className="text-ctp-subtext0 text-center justify-center items-center flex">
-                  <div className="hidden group-hover/card:flex justify-center mr-1">
-                    <NumberInputWithButtons
-                      count={currentEpisode || 0}
-                      setCount={(newCount) => {
-                        setCurrentEpisode(id, newCount);
-                      }}
-                      maxCount={episodes}
-                    />
-                  </div>
-                  <div className="group-hover/card:hidden mr-1">
-                    {currentEpisode}
-                  </div>
-                  of {episodes || "?"}
-                </div>
-              )
-            )}
-            <StatusButton
-              currentStatus={currentStatus}
-              handleStatusChange={handleStatusChange}
-              buttonText={getButtonText()}
-            />
-          </div>
+    <MediaCard coverImage={cover_image} alt={title}>
+      <h2 className="font-bold line-clamp-2 leading-tight" title={title}>
+        {title}
+      </h2>
+      <p className="text-sm mb-1">{studio}</p>
+      <p>
+        {release_season} {year}
+      </p>
+      <div
+        className={
+          score >= 8
+            ? "text-ctp-green"
+            : score >= 6
+            ? "text-ctp-yellow"
+            : score >= 5
+            ? "text-ctp-maroon"
+            : score === null
+            ? "text-ctp-subtext0"
+            : "text-ctp-red"
+        }
+      >
+        <div className="flex items-center gap-1">
+          {score || "N/A"} <FaStar />
         </div>
       </div>
-    </div>
+      <p>
+        {episodes || "N/A"} {videoType === "TV" ? "Episodes" : videoType}
+      </p>
+
+      <div className="h-12 overflow-hidden mt-1">
+        <div className="flex flex-wrap gap-1">
+          {themes?.map((theme: string) => (
+            <div
+              key={theme}
+              className="text-xs bg-ctp-surface2 rounded-xl w-fit px-2 py-0.5"
+            >
+              {theme}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 w-full">
+        {currentStatus === "completed" ? (
+          <div className="text-ctp-subtext0 text-center">
+            Finished {dateAdded}
+          </div>
+        ) : (
+          currentStatus === "progress" && (
+            <div className="text-ctp-subtext0 text-center justify-center items-center flex">
+              <div className="hidden group-hover/card:flex justify-center mr-1">
+                <NumberInputWithButtons
+                  count={currentEpisode || 0}
+                  setCount={(newCount) => {
+                    setCurrentEpisode(id, newCount);
+                  }}
+                  maxCount={episodes}
+                />
+              </div>
+              <div className="group-hover/card:hidden mr-1">
+                {currentEpisode}
+              </div>
+              of {episodes || "?"}
+            </div>
+          )
+        )}
+        <StatusButton
+          currentStatus={currentStatus}
+          handleStatusChange={handleStatusChange}
+          buttonText={getButtonText()}
+        />
+      </div>
+    </MediaCard>
   );
 }
 
